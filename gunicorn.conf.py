cefd4236d032f1d18b55e8d406936b98c76ed69f
Ertwrx <erwinmgregorio@gmail.com>
2025-04-23 05:02:57 +0400

    restructure

--- gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-# gunicorn.conf.py
-
-bind = "0.0.0.0:5000"  # Set the host and port for Gunicorn
-workers = 4            # Number of worker processes
-threads = 2            # Number of worker threads per process
-worker_class = "gthread"  # Worker processes class
-timeout = 120           # Request timeout in seconds
-preload_app = True      # Preload the application before forking worker processes
